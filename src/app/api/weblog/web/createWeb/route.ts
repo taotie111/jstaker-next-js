16040b0e2b535888a82cebe18c5fe40335cfdfc2
niyaowei <1023931586@qq.com>
2024-05-26 22:29:38 +0800

    部分完成web站点管理

---
+++ src/app/api/weblog/web/createWeb/route.ts
@@ -0,0 +1,76 @@
+import { NextResponse,NextRequest } from "next/server";
+import {PrismaClient} from "@prisma/client"
+import {deleteParamsIsNotNull} from "../../../../utils/apiUtils"; 
+
+
+const prisma = new PrismaClient();
+
+export const OPTIONS = (req: NextRequest, res: NextResponse) => {
+    // res.status(200).send(); // 响应 OPTIONS 请求
+    const response = NextResponse.json({
+        success: true,
+        errorMessage: '',
+        data: {}
+    });
+
+    response.headers.set('Access-Control-Allow-Origin', '*'); // 允许任何来源的请求
+    response.headers.set('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE'); // 允许的 HTTP 方法
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // 允许的请求头
+
+    return response;
+};
+export const GET = async (
+    req: NextRequest
+) => {
+    const json = await req.json();
+
+    await prisma.$connect();
+    const createMany = await prisma.web.createMany({
+        data: [
+
+        ]
+    }); 
+    await prisma.$disconnect();
+    return NextResponse.json({
+        success: true,
+        errorMessage:'',
+        data:createMany
+    })
+}
+export interface web_data {
+    name: string;
+    token: string;
+    remake: string;
+    roleLevel: string
+}
+
+export  const  POST = async (
+    req: NextRequest,
+    {params} : any
+) => {
+    const json = await req.json();
+    const data: web_data = {
+        name: json.name,
+        token: json.token,
+        remake: json.remake,
+        roleLevel: json.roleLevel
+    }
+    await prisma.$connect();
+    const createMany = await prisma.web.createMany({
+        data: [
+            data
+        ]
+    }); 
+    await prisma.$disconnect();
+    return NextResponse.json({
+        success: true,
+        errorMessage:'',
+        data:createMany
+    })
+    return NextResponse.json({
+        success: true,
+        errorMessage: '',
+        data: {}
+    })
+}
+
