51f805d6edc6ebe92d24e1503d25eea3f2e43b22
niyaowei <1023931586@qq.com>
2024-06-06 19:55:22 +0800

    feat: 将monitoringScreen

--- src/app/jstakerDashBoard/monitoringScreen/page.jsx
+++ src/app/jstakerDashBoard/monitoringScreen/page.jsx
@@ -1,16 +1,12 @@
-// "use client"
 // import 'server-only'
 import styles from './styles.module.css'
-// import { useAtomValue, } from 'jotai'
-import {checkWebListAtom} from "@/app/store/webListData/state.js"
+
 export default function MonitoringScreen(){
-    console.log(checkWebListAtom.toString());
-    // const webList = useAtomValue(checkWebListAtom)
-    // console.log(webList)
+
     return (
         <div className={styles.monitoringScreen} >
             <div className={styles.titleLine}>
-                
+                {{webList}}
             </div>
             <div className={styles}>
                 <div>
